9f81fd092f7b0cab0ca56e10588c3b74aec56ca9
Jaylen Thomas <jaylenthomas@Jaylens-MacBook-Pro.local>
2025-01-01 11:09:33 -0500

    coding challenge 1 & 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,37 @@
 console.log('hello world');
+let name = 'Jaylen';
+console.log(`hello ${name} welcome to JS`);
+/* 
+JS Primitive types:
+    -Number
+    -String
+    -Boolean
+    -Undefined
+    -Null
+    -BigInt
+    -Symbol
+*/
+
+// challenge #1: caclulate BMI
+const caclulateBMI = (mass, height) => {
+  return Math.round(mass / (height * height));
+};
+// Mark
+let massMark = 78;
+let heightMark = 1.69;
+const markBMI = caclulateBMI(massMark, heightMark);
+// John
+let massJohn = 92;
+let heightJohn = 1.95;
+const johnBMI = caclulateBMI(massJohn, heightJohn);
+
+console.log(markBMI > johnBMI ? 'Mark BMI is higher' : 'John BMI is higher');
+
+// challenge #2:
+console.log(
+  `${markBMI > johnBMI ? "Mark's" : "John's"} BMI ${
+    markBMI > johnBMI ? '(' + markBMI + ')' : '(' + johnBMI + ')'
+  } is higher than ${markBMI < johnBMI ? "Mark's" : "John's"} BMI ${
+    markBMI < johnBMI ? '(' + markBMI + ')' : '(' + johnBMI + ')'
+  }`
+);
